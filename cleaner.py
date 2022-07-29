992a8d184f9d943e87804b7e14b0b7bdf045a7df
Aniket <vyas45@gmail.com>
2022-07-29 11:39:33 +0530

    Options Handling and ReadMe Update

--- cleaner.py
+++ cleaner.py
@@ -11,15 +11,23 @@ Todo:
 '''
 import os
 import shutil
+import argparse
+
+# Setup the class to handle the metadata for the utility
+class DirSegger:
+    def __init__(self, dir_name):
+        self.dir = dir_name
 
 def makeExtensionMap(dir):
    retMap = {'noext':[]}
    extList = []
 
    #omit diretories
-   file_list = [f for f in os.listdir(dir) if os.path.isfile(f)]
+   file_list = [f for f in os.listdir(dir) if os.path.isfile(dir + "/" + f)]
+   print(file_list)
 
    # Populate the extension map
+   # TODO: Find a better way to get extension on files
    for file in file_list:
       ext_split = file.split(".")
       # Add to the noext direcory
@@ -52,18 +60,37 @@ def moveFiles(path, ext_map):
             print(src, dest)
             shutil.move(src, dest)
             
-def main():
+def main(dir):
    ext_map = {}
 
    # Get the list of extensions in the current directory
-   dir = os.getcwd()
+   #dir = os.getcwd()
+   print("Dir is : ", dir)
    ext_map = makeExtensionMap(dir) 
 
+   print(ext_map)
+   
    # Walk over the extension map to create the diretory 
    createDirs(dir, list(ext_map.keys()))
 
    # Move the files to respective directories
    moveFiles(dir, ext_map)
 
+# Handle the init stuff
+def init():
+    parser = argparse.ArgumentParser( description='Descirbes the options for DirectorySegger')
+    parser.add_argument("--dir", type=str, required=False, help='The directory which is getting cleaned')
+    args = parser.parse_args()
+
+    if args.dir:
+        dir_name = args.dir
+    else:
+        dir_name = os.getcwd() 
+
+    # Setup the class object
+    dirSeg = DirSegger(dir_name)        
+    return (dirSeg)
+
 if __name__=="__main__":
-    main()
+    dirSeg = init()
+    main(dirSeg.dir)
